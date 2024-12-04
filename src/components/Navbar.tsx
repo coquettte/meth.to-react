d1957fd94098f3b90c21db3554325a047349eba2
Drifter <root@coquettte.com>
2024-12-04 22:03:33 +0200

    Add Discord Invite

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -57,7 +57,7 @@ export function Navbar() {
                     <NavigationMenuTrigger className="transition-colors hover:text-foreground/80 text-foreground/60">Links</NavigationMenuTrigger>
                     <NavigationMenuContent>
                         <ul className="grid gap-3 p-4 md:w-[400px] lg:w-[500px] lg:grid-cols-[.75fr_1fr]">
-                            <li className="row-span-3">
+                            <li className="row-span-4">
                                 <NavigationMenuLink asChild>
                                     <a
                                         className="flex h-full w-full select-none flex-col justify-end rounded-md bg-gradient-to-b from-muted/50 to-muted no-underline outline-none focus:shadow-md"
@@ -75,6 +75,9 @@ export function Navbar() {
                             </ListItem>
                             <ListItem href="https://forum.meth.su/" title="Forum">
                                 Meth Forum
+                            </ListItem>
+                            <ListItem href="https://discord.gg/e7tmuuk9At" title="Discord">
+                                Discord Server
                             </ListItem>
                         </ul>
                     </NavigationMenuContent>
