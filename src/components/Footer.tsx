10a464b1391305a0c7e0442dd0b94e8831698375
Drifter <root@coquettte.com>
2024-12-04 17:28:09 +0200

    old meth su link

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,7 +6,7 @@ export function ModernFooter() {
             <div className="container flex flex-col items-center justify-between gap-4 py-10 md:h-24 md:flex-row md:py-0 mx-auto">
                 <div className="flex flex-col items-center gap-4 px-8 md:flex-row md:gap-2 md:px-0">
                     <p className="text-center text-sm leading-loose text-muted-foreground md:text-left">
-                        The owners of this site are not liable for any harm caused by the information on this website, which exists solely as an educational resource for researchers and scholars. Additionally, it is important to note that the recipes provided on this site are purely fictional and for entertainment purposes only.
+                        The owners of this site are not liable for any harm caused by the information on this website, which exists solely as an educational resource for researchers and scholars. Additionally, it is important to note that the recipes provided on this site are purely fictional and for entertainment purposes only. Old meth.su<a className='text-neutral-400 hover:text-neutral-500' href='https://old.meth.su'> here</a>.
                     </p>
                 </div>
                 <div className="flex gap-4">
